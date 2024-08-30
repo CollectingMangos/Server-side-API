ce6c7684ad87bd221dfe8b02c3290b62e5fdb7f0
Ruben <rubenfds@icloud.com>
2024-08-30 22:20:17 +0200

    added in error message so string cannot be empty

--- endPoint.py
+++ endPoint.py
@@ -2,9 +2,13 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/sort-string', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def sort_string():
     data = request.json.get('data', '')
+    
+    if not data:
+        return jsonify({'error':'the string cannot be empty'})
+
     array_of_characters = list(data)
     array_of_characters.sort()
     return jsonify({'word': array_of_characters})
