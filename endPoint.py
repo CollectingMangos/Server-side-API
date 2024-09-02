a95e1aeec046da7462bdf1de8a143947ed8edc0e
Ruben <rubenfds@icloud.com>
2024-09-02 14:58:44 +0200

    catered for empty strings and live testing

--- endPoint.py
+++ endPoint.py
@@ -3,15 +3,18 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
-def sort_string():
-    data = request.json.get('data', '')
+def sort():
+    string_data = request.json.get('data', '')
     
-    if not data:
-        return jsonify({'error':'the string cannot be empty'})
-
-    array_of_characters = list(data)
+    if not string_data:
+        return jsonify({'error':'if you see this, your string cannot be empty'}), 400
+    
+    array_of_characters = list(string_data)
     array_of_characters.sort()
+    
     return jsonify({'word': array_of_characters})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+# Uncommen the below if you want to run tests locally 
+# if __name__ == '__main__':
+#     app.run(debug=True)
